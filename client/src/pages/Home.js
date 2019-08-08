69cb5bf17873a3f1a8a82afc126973a28ca787e8
Triumph <archie200@icloud.com>
2019-08-08 14:33:44 -0500

    current users

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -39,7 +39,7 @@ class Home extends Component {
 <Row>
     <Col><TopicMenu /></Col>
     <Col xs={6}>2 of 3</Col>
-    <Col>3 of 3</Col>
+    <Col><CurrentUsers /></Col>
 </Row>
                  
                 <Footer />
